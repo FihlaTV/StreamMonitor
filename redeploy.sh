4cc5efb84042aee8671069aaf18dbd8757a6002c
mekya <ahmetmermerkaya@gmail.com>
2018-12-28 17:38:28 +0300

    Initial commit

---
+++ redeploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mvn clean install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true -DskipITs -Dgpg.skip=true
+
+USER_HOME_DIR=/Users/mypc
+
+SRC=target/StreamMonitor.war
+
+DEST=~/softwares/ant-media-server/webapps/
+
+
+#copy red5 jar from target dir to red5 dir
+
+rm -rf $DEST/StreamMonitor
+cp  $SRC  $DEST
+
+
+
+#go to red5 dir
+cd ~/softwares/ant-media-server
+
+#shutdown red5 
+./stop.sh
+
+
+#start red5
+./start-debug.sh
+
---
+++ src/main/java/io/antmedia/app/StreamMonitorApplication.java
@@ -0,0 +1,27 @@
+package io.antmedia.app;
+
+import org.red5.server.api.IConnection;
+import org.red5.server.api.scope.IScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.antmedia.AntMediaApplicationAdapter;
+
+/**
+ * Sample application that uses the client manager.
+ * 
+ * @author The Red5 Project (red5@osflash.org)
+ */
+public class StreamMonitorApplication extends AntMediaApplicationAdapter {
+
+	protected static Logger logger = LoggerFactory.getLogger(StreamMonitorApplication.class);
+
+	public static final String STORAGE_FORWARD_URL = "https://s3.eu-central-1.amazonaws.com/";
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean connect(IConnection conn, IScope scope, Object[] params) {
+		return true;
+	}
+
+}
